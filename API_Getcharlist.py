9837df1415be0e3018188b2561117bb4866cd969
k1101jh <h1103095@gmail.com>
2023-09-16 00:57:16 +0900

    Read Json data

--- API_Getcharlist.py
+++ API_Getcharlist.py
@@ -3,42 +3,41 @@ import requests
 import json
 
 headers = {
-    'accept': 'application/json',
-    'authorization': 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAzMjEyODgifQ.G2e5bMouPV4Nwnab1lPMnhJ25YNHwAMcDL9rNWEcxoN_Cn4o4QLVrtQg9ssXZoexaKXU4j_i9HDxV8_9sWUlfFysIDd16aGdU9xKetJMI4owzXPNX7A4SUTo-VhgUCUxT4PnNUXXEQyUW5T3Wce_0K1LuqzlSzgzYGAozHQG1Pb4Uf1gPKs6lbp5oxwNp1aquu7vcTBeVnV6MYN5G-efSyrs8J4kUaeScYjIThu3npu4ZUFpJAAOzHgVGySSkujdXNuvpB-aSA_dAqYP_7Cnl5gwzPUHwyx9C5A0gpNdclyZVFHSm-NIT4apXj29MCQBGpmTxMB5vOEUHsH44RrM6g'
+    "accept": "application/json",
+    "authorization": "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAzMjEyODgifQ.G2e5bMouPV4Nwnab1lPMnhJ25YNHwAMcDL9rNWEcxoN_Cn4o4QLVrtQg9ssXZoexaKXU4j_i9HDxV8_9sWUlfFysIDd16aGdU9xKetJMI4owzXPNX7A4SUTo-VhgUCUxT4PnNUXXEQyUW5T3Wce_0K1LuqzlSzgzYGAozHQG1Pb4Uf1gPKs6lbp5oxwNp1aquu7vcTBeVnV6MYN5G-efSyrs8J4kUaeScYjIThu3npu4ZUFpJAAOzHgVGySSkujdXNuvpB-aSA_dAqYP_7Cnl5gwzPUHwyx9C5A0gpNdclyZVFHSm-NIT4apXj29MCQBGpmTxMB5vOEUHsH44RrM6g",
 }
 
 # 디렉터리 경로 생성 (./members/)
-directory = './members/'
-if not os.path.exists(directory):
-    os.makedirs(directory)
+directory = "./members/"
+os.makedirs(directory, exist_ok=True)
 
 while True:
     # 사용자로부터 이름과 캐릭터 이름을 입력 받음
     name = input('이름을 입력하세요 (입력 종료하려면 "x"를 입력하세요): ')
-    
-    if name.lower() == 'x':
+
+    if name.lower() == "x":
         break  # "x" 입력 시 입력 종료
 
-    character_name = input('캐릭터 이름을 입력하세요: ')
+    character_name = input("캐릭터 이름을 입력하세요: ")
 
     # URL 생성
-    url = f'https://developer-lostark.game.onstove.com/characters/{character_name}/siblings'
+    url = f"https://developer-lostark.game.onstove.com/characters/{character_name}/siblings"
 
     response = requests.get(url, headers=headers)
 
     # 올바른 방법으로 HTTP 응답 상태 코드를 확인
     if response.status_code == 200:
-        print('요청 상태: 정상 작동')
+        print("요청 상태: 정상 작동")
         data = response.json()
 
         if data:
-            file_name = f'{name}.json'
-            with open(os.path.join(directory, file_name), 'w', encoding='utf-8') as txt_file:
-                txt_file.write(json.dumps(data, ensure_ascii=False, indent=4))
+            file_name = f"{name}.json"
+            with open(os.path.join(directory, file_name), "w", encoding="utf-8") as file:
+                file.write(json.dumps(data, ensure_ascii=False, indent=4))
 
-            print(f'데이터를 {file_name} 파일로 저장했습니다.')
+            print(f"데이터를 {file_name} 파일로 저장했습니다.")
         else:
-            print('데이터가 없습니다.')
+            print("데이터가 없습니다.")
     else:
-        print('요청 상태: 기타 오류')
+        print("요청 상태: 기타 오류")
         continue
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
 import itertools
 import statistics
+import json
+
 
 def get_position(job):
     support_jobs = ["바드", "도화가", "홀리나이트"]
     return "서폿" if job in support_jobs else "딜러"
+
 
 def get_synergy(job):
     synergies = {
@@ -13,11 +17,12 @@ def get_synergy(job):
         "받피증": ["소울이터", "소서리스", "버서커", "데모닉", "호크아이", "인파이터", "슬레이어"],
         "방감": ["워로드", "서머너", "블래스터", "디스트로이어", "리퍼"],
         "사멸": ["디스트로이어", "워로드", "블레이드", "인파이터", "스트라이커", "데빌헌터", "블레이드", "리퍼", "창술사", "슬레이어"],
-        "백헤드": ["워로드", "블레이드"]
+        "백헤드": ["워로드", "블레이드"],
     }
 
     char_synergies = [k for k, v in synergies.items() if job in v]
     return char_synergies
+
 
 def form_party(members, raid_party_name, party_size=4):
     if raid_party_name == "쿠크세이튼":
@@ -33,16 +38,18 @@ def form_party(members, raid_party_name, party_size=4):
             if support_count != 1:
                 continue
 
-            if len(set(char[0] for char in party)) != len(party) or \
-               len(set(char[1] for char in party)) != len(party) or \
-               len(set(char[2] for char in party)) != len(party):
+            if (
+                len(set(char[0] for char in party)) != len(party)
+                or len(set(char[1] for char in party)) != len(party)
+                or len(set(char[2] for char in party)) != len(party)
+            ):
                 continue
 
             party_synergies = list(set(itertools.chain(*[get_synergy(char[2]) for char in party])))
 
             non_support_levels = [char[3] for char in party if get_position(char[2]) != "서폿"]
             average_level_non_support = sum(non_support_levels) / len(non_support_levels)
-            
+
             total_average_level = sum(char[3] for char in party) / len(party)
             valid_parties.append((total_average_level, average_level_non_support, party_synergies, party))
 
@@ -54,25 +61,38 @@ def form_party(members, raid_party_name, party_size=4):
         valid_parties.sort(key=lambda x: abs(x[0] - target_level))
         chosen_party = valid_parties[0]
         possible_parties.append(chosen_party)
-        
+
         for char in chosen_party[3]:
             members.remove(list(char))
             used_characters.add(tuple(char))
 
     return possible_parties
 
+
 def main():
     raid_party_name = input("어떤 파티를 구성하실 건가요? (예: 발탄, 비아키스, 쿠크세이튼, 아브렐슈드, 일리아칸, 카양갤, 상아탑, 카멘): ")
-    input_data = input("\n캐릭터 정보를 한번에 입력하세요 ([이름, 닉네임, 전투력, 레벨] 형식으로 공백으로 구분): ")
-    data_list = input_data.split(' ')
+
+    # 멤버 파일 읽어들이기
+    member_dir_path = "./members"
+    member_file_list = os.listdir(member_dir_path)
+
+    member_data_dict = {}
     characters = []
 
-    for i in range(0, len(data_list), 4):
-        name, nickname, job, level = data_list[i], data_list[i+1], data_list[i+2], int(data_list[i+3])
-        characters.append([name, nickname, job, level])
+    for member_file in member_file_list:
+        name = os.path.splitext(member_file)[0]
+        with open(os.path.join(member_dir_path, member_file), "r", encoding="UTF-8") as f:
+            member_data_dict[name] = json.load(f)
+
+    for name, member_data in member_data_dict.items():
+        for character_data in member_data:
+            character_name = character_data["CharacterName"]
+            character_class_name = character_data["CharacterClassName"]
+            item_max_level = int(float(character_data["ItemMaxLevel"].replace(",", "")))
+            characters.append([name, character_name, character_class_name, item_max_level])
 
     parties = form_party(characters, raid_party_name)
-    
+
     for idx, (total_avg, non_support_avg, synergies, party) in enumerate(parties, 1):
         print(f"\n{raid_party_name} 파티 {idx} (파티 시너지 : {', '.join(synergies)}):")
         for char in party:
@@ -80,14 +100,22 @@ def main():
         print(f"전체 평균 레벨: {total_avg:.2f}")
         print(f"서폿 제외 평균 레벨: {non_support_avg:.2f}")
 
-    unused_characters = [char for char in characters if tuple(char) not in set(tuple(item) for sublist in parties for item in sublist[3])]
+    unused_characters = [
+        char
+        for char in characters
+        if tuple(char) not in set(tuple(item) for sublist in parties for item in sublist[3])
+    ]
 
     if unused_characters:
         print("\n사용되지 않은 캐릭터:")
 
-        level_too_high = [char for char in unused_characters if raid_party_name == "쿠크세이튼" and char[3] >= 1580]
+        level_too_high = [
+            char for char in unused_characters if raid_party_name == "쿠크세이튼" and char[3] >= 1580
+        ]
         level_too_low = [char for char in unused_characters if raid_party_name == "쿠크세이튼" and char[3] < 1475]
-        no_combination = [char for char in unused_characters if char not in level_too_high and char not in level_too_low]
+        no_combination = [
+            char for char in unused_characters if char not in level_too_high and char not in level_too_low
+        ]
 
         if level_too_high:
             print("\n사유 : 권장 레벨 초과(골드 손해):")
@@ -103,6 +131,7 @@ def main():
             print("\n사유 : 적절한 조합을 찾지 못함:")
             for char in no_combination:
                 print(char)
+
 
 if __name__ == "__main__":
     main()
